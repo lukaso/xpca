77b654c7f847cfec685a2f47cebbdd853d4b7a34
Peter Vandenberk <pvandenberk@mac.com>
2011-06-20 08:45:44 +0100

    specs for the basic PostcodeAnywhere behaviour: initialization and configuration

--- spec/postcode_anywhere_spec.rb
+++ spec/postcode_anywhere_spec.rb
@@ -2,4 +2,50 @@ require 'spec_helper'
 
 describe PostcodeAnywhere do
 
+  describe "the PostcodeAnywhere initializer" do
+
+    it "takes an options hash as its argument" do
+      lambda { PostcodeAnywhere.new({}) }.should_not raise_error ArgumentError
+    end
+
+    it "uses an empty hash as its default argument" do
+      lambda { PostcodeAnywhere.new() }.should_not raise_error ArgumentError
+    end
+
+  end
+
+  describe "the PostcodeAnywhere account_code method" do
+
+    it "should be implemented" do
+      PostcodeAnywhere.new.should respond_to(:account_code).with(0).arguments
+    end
+
+    it "should return nil by default" do
+      PostcodeAnywhere.new.account_code.should be nil
+    end
+
+    it "should configure itself from the options hash" do
+      acc_code = "XXXXX00000"
+      PostcodeAnywhere.new(:account_code => acc_code).account_code.should eq acc_code
+    end
+
+  end
+
+  describe "the PostcodeAnywhere license_key method" do
+
+    it "should be implemented" do
+      PostcodeAnywhere.new.should respond_to(:license_key).with(0).arguments
+    end
+
+    it "shoud return nil by default" do
+      PostcodeAnywhere.new.license_key.should be nil
+    end
+
+    it "should configure itself from the options hash" do
+      lic_key = "AA00-BB11-CC22-DD33"
+      PostcodeAnywhere.new(:license_key => lic_key).license_key.should eq lic_key
+    end
+
+  end
+
 end
